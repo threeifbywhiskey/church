3989e335c1b57cd09616e80e97d02e54b33d2de7
Three If By Whiskey <3ifbyw@gmail.com>
2014-03-18 09:12:07 -0400

    Add SQRT, FLOOR, and PRIME procedures

--- lib/church/math.rb
+++ lib/church/math.rb
@@ -1,42 +1,11 @@
 module Church
-  CMP = -> a, b {
-    a > b ? 1 : a < b ? -1 : 0
-  }
+  SQRT = -> n { n ** 2 ** -1 }
 
-  ADD = -> a, b {
-    b == 0 ? a : ADD[a + CMP[b, 0], b + CMP[0, b]]
-  }
+  FLOOR = -> n { n - n % 1 }
 
-  SUB = -> a, b {
-    b == 0 ? a : SUB[a + (0 <=> b), b + (0 <=> b)]
-  }
-
-  MUL = -> a, b {
-    (mul_p = -> c, d {
-      d == b ? c + a * CMP[0, b] : mul_p[c + a * CMP[b, 0], d + CMP[b, 0]]
-    })[a * CMP[b, 0], 0]
-  }
-
-  DIV = -> a, b {
-    div_p = -> d {
-      d = ADD[d, CMP[b, 0]]
-      a = SUB[a, MUL[b, CMP[b, 0]]]
-      a >= MUL[b, CMP[b, 0]] ? div_p[d] : d
-    }
-    b > a ? 0 : div_p[0]
-  }
-
-  MOD = -> a, b {
-    MOD_p = -> {
-      a = SUB[a, MUL[b, CMP[b, 0]]]
-      a > b ? MOD_p[] : (b < 0 ? SUB[a, b] : a)
-    }
-    MOD_p[]
-  }
-
-  POW = -> a, b {
-    (POW_p = -> c, d {
-      d == b ? c : POW_p[MUL[a, c], ADD[d, 1]]
-    })[1, 0]
+  PRIME = -> n {
+    n = n < 0 ? -n : n
+    n < 3 ? n == 2 : n % 2 == 0 ? false :
+    REDUCE[[1] + MAP[[*3..FLOOR[SQRT[n]]], &-> m { n % m }], &:*] > 0
   }
 end
---
+++ spec/math_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'prime'
+
+include Church
+
+describe 'SQRT' do
+  it "should return its argument's square root" do
+    expect(SQRT[5]).to eq Math.sqrt 5
+  end
+end
+
+describe 'FLOOR' do
+  it "should round its argument down" do
+    expect(FLOOR[2.5]).to eq 2
+  end
+end
+
+describe 'PRIME' do
+  (-25..25).each do |i|
+    it "should return its argument's primality" do
+      expect(PRIME[i]).to be i.prime?
+    end
+  end
+end
