a2ea8a38419668d7beabd0ce6f42b2ab9e1689fd
Three If By Whiskey <3ifbyw@gmail.com>
2014-03-19 02:39:25 -0400

    Use simplecov-console for nicer console output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::Console
+]
+
 SimpleCov.start
 
 require File.expand_path('../../lib/church', __FILE__)
