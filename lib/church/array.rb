0c8619fda6aa0a46584351303fd0fdebfd3a33e7
Three If By Whiskey <3ifbyw@gmail.com>
2014-03-19 00:01:30 -0400

    Remove superfluous iterative procedures
    FILTER, MAP, and SIZE had iterative copies that did the same thing but
    would have required redundant testing for 100% coverage by SimpleCov.

--- lib/church/array.rb
+++ lib/church/array.rb
@@ -4,30 +4,9 @@ module Church
     coll == [] || coll == '' ? 0 : 1 + SIZE[coll[1..-1]]
   }
 
-  # Returns the size of an array or string using iteration.
-  ISIZE = -> coll {
-    sz = 0
-    (slicer = -> {
-      sz += 1
-      (coll = coll[1..-1]) == [] || coll == '' ? sz : slicer[]
-    })[]
-  }
-
   # Maps fn over coll recursively.
   MAP = -> coll, &fn {
     coll == [] ? [] : [fn[coll[0]]] + MAP[coll[1..-1], &fn]
-  }
-
-  # Maps fn over coll iteratively.
-  IMAP = -> coll, &fn {
-    sz = SIZE[coll]
-    ret = []
-    i = 0
-
-    (mapper = -> {
-      ret[i] = fn[coll[i]]
-      (i += 1) == sz ? ret : mapper[]
-    })[]
   }
 
   # Maps fn over coll iteratively and in-place, modifying the original collection.
@@ -56,18 +35,6 @@ module Church
   # Filters coll through fn using recursion.
   FILTER = -> coll, &fn {
     coll == [] ? [] : (fn[coll[0]] ? [coll[0]] : []) + FILTER[coll[1..-1], &fn]
-  }
-
-  # Filters coll through fn using iteration.
-  IFILTER = -> coll, &fn {
-    sz = SIZE[coll]
-    ret = []
-    i = 0
-
-    (filterer = -> {
-      fn[coll[i]] ? ret << coll[i] : 0
-      (i += 1) == sz ? ret : filterer[]
-    })[]
   }
 
   # Performs fn with each element of coll in turn.
